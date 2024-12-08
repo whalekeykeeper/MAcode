31bbb77d8465a840102e5da61d5f6fb2060fc256
Qin Gu <guqin7@gmail.com>
2024-12-08 21:31:51 +0100

    use Gemini API instead of OpenAI API

--- watch-backend/app/core/translation_service.py
+++ watch-backend/app/core/translation_service.py
@@ -1,23 +1,56 @@
+# import openai
 import re
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
 
-import openai
+api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
+    print("Error: GEMINI_API_KEY is not set")
+else:
+    print("API Key loaded successfully!")
 
+# Get the Gemini API key from environment variables
+genai.configure(api_key=api_key)
 
+# ## The following code is using OpenAI API
+# def translate(word: str, sentence: str) -> str:
+#     completion = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": "You are a helpful assistant who is good on math.",
+#             },
+#             {"role": "user",
+#              "content": "You are a experienced translator for English and Chinese. Detect the given language, "
+#                         "and translate it into the other language among English and Simplified Chinese. Then translate given word \"{}\" in sentence \"{}\". Keep the translation short.".format(word, sentence)},
+#         ],
+#     )
+#     return completion.choices[0].message.content
+
+## The following code is using Google Gemini API
 def translate(word: str, sentence: str) -> str:
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a helpful assistant who is good on math.",
-            },
-            {"role": "user",
-             "content": "You are a experienced translator for English and Chinese. Detect the given language, "
-                        "and translate it into the other language among English and Simplified Chinese. Then translate given word \"{}\" in sentence \"{}\". Keep the translation short.".format(word, sentence)},
-        ],
-    )
-    return completion.choices[0].message.content
+    word = _preprocess_word(word)
+    # Define the prompt for translation
+    # ToDo(Qin): Improve the prompt
+    prompt = (f"Translate the word \"{word}\" in the context of the sentence \"{sentence}\" from English to simplified "
+              f"Chinese or vice versa. Keep the translation short and ensure the translation is " \
+             f"accurate and contextually appropriate. Provide the translated word in written format only, "
+              f"not the entire sentence, and also not the pinyin.")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+    translated_text = response.text.strip()
+    print("Response: ", response)
+    # For debugging
+    print("----\nword: \"{}\"\nsentence: \"{}\"\ntranslation: {}\n".format(word, sentence, translated_text))
+    return translated_text
 
+# Remove punctuations at the beginning or the end of the word. The word is either in English or Simplified Chinese.
+def _preprocess_word(word: str) -> str:
+    word = word.strip(".,?!\"'()[]{}:;").strip()
+    return word
 
 def stat(text: str) -> []:
     sentences = re.split("[?.,!]", text)
