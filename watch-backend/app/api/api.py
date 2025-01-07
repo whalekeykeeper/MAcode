6df1703258658154abd0b34724e6366433a7b805
Qin Gu <guqin7@gmail.com>
2025-01-07 15:03:51 +0100

    fix: fix typo

--- watch-backend/app/api/api.py
+++ watch-backend/app/api/api.py
@@ -5,7 +5,7 @@ from app.api.endpoints import users, video
 api_router = APIRouter()
 # api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(video.router, prefix="/video", tags=["video"])
+api_router.include_router(video.router, prefix="/videos", tags=["videos"])
 # api_router.include_router(
 #     translation.router, prefix="/translation", tags=["translation"]
 # )
