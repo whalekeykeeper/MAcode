1a16f01ff205b9b962711866ec98ff0ee4453c89
Qin Gu <guqin7@gmail.com>
2025-01-13 20:42:58 +0100

    feat: implement the chosen_words endpoint for marking words as learned

--- watch-backend/app/api/api.py
+++ watch-backend/app/api/api.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
-from app.api.endpoints.video import router as video_router
-from app.api.endpoints.word_list_collection import router as word_list_router
+
 from app.api.endpoints.gap_filling_exercises import router as gap_filling_router
-from app.api.endpoints.visualization import router as visualization_router
 from app.api.endpoints.users import router as users_router
+from app.api.endpoints.video import router as video_router
+from app.api.endpoints.visualization import router as visualization_router
+from app.api.endpoints.chosen_words import router as chosen_words_router    
+
 api_router = APIRouter()
 
 # Include all endpoint routers
 api_router.include_router(video_router, prefix="/videos", tags=["videos"])
-api_router.include_router(word_list_router, prefix="/word_list", tags=["word_list"])
+api_router.include_router(chosen_words_router, prefix="/chosen_words", tags=["chosen_words"])
 api_router.include_router(gap_filling_router, prefix="/gap_filling", tags=["gap_filling"])
 api_router.include_router(visualization_router, prefix="/visualization", tags=["visualization"])
 api_router.include_router(users_router, prefix="/users", tags=["users"])
---
+++ watch-backend/app/api/endpoints/chosen_words.py
@@ -0,0 +1,118 @@
+from fastapi import APIRouter, Depends, HTTPException, Header, Body
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api import deps
+from app.core.logger import logger
+from app.models import ChosenWords, GraphNode, User
+
+"""
+This file contains endpoints to show a collection of words as a table. Word data is sent with word id.
+This table shows (checkbox, word, word_in_another_language_from_bilingual_subtitle, sentence) in a table with 4 columns.
+The words are fetched from the database from the "chosen_words" table.
+The order is from the least mastery score. If the mastery score is the same, the order is from the earliest chosen time.
+The checkbox "marked_as_learned" is used to let the user eliminate the words from generating exercises manually.
+"""
+
+router = APIRouter()
+
+
+@router.get("/")
+async def get_word_list(
+        uuid: str = Header(...),
+        session: AsyncSession = Depends(deps.get_session),
+):
+    """
+    A function to get the word list from the database
+    """
+    stmt = select(User).where(User.uuid == uuid)
+    user = (await session.execute(stmt)).scalar_one_or_none()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid user")
+    user_id = user.id
+
+    try:
+        # Fetch words from ChosenWords table when its "marked_as_learned" field is False,
+        # Order from the latest record time.
+        # If time is the same, then order from the least mastery score.
+        stmt = select(ChosenWords).where(ChosenWords.user_id == user_id)
+        chosen_word_list = (await session.execute(stmt)).scalars().all()
+        logger.debug(f"The number of chosen words is {len(chosen_word_list)}")
+        display_word_list = []
+        for chosen_word in chosen_word_list:
+            if chosen_word.marked_as_learned:  # If the word is marked as learned, skip it.
+                continue
+
+            stmt = select(GraphNode).where(GraphNode.lemma == chosen_word.lemma)
+            graph_node = (await session.execute(stmt)).scalar_one_or_none()
+
+            display_word_list.append(
+                (chosen_word.word_id, chosen_word.marked_as_learned, chosen_word.lemma, chosen_word.translation,
+                 chosen_word.sentence,
+                 graph_node.mastery, chosen_word.record_time))
+            # logger.debug(
+            #     f"Add word {chosen_word.lemma} with id {chosen_word.word_id} "
+            #     f"and translation {chosen_word.translation} to the display word list.")
+
+        # Sort the list by record time (from latest on) and mastery score (from least on)
+        display_word_list.sort(key=lambda x: (-x[5], x[6]))
+
+        return display_word_list
+    except Exception as e:
+        logger.error(f"Could not fetch the chosen words' list from ChosenWords table: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+@router.post("/acquired")
+async def mark_as_required(
+        word_id: int = Body(..., embed=True),
+        uuid: str = Header(...),
+        session: AsyncSession = Depends(deps.get_session),
+) -> None:
+    """
+    A function to mark the chosen words as acquired
+    """
+    stmt = select(User).where(User.uuid == uuid)
+    user = (await session.execute(stmt)).scalar_one_or_none()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid user")
+    logger.debug(f"User {user.uuid} is found with id {user.id}")
+
+    try:
+        logger.debug(f"-----------------Marking word {word_id} as acquired-----------------")
+        stmt = select(ChosenWords).where(ChosenWords.word_id == word_id)
+        chosen_word = (await session.execute(stmt)).scalar_one_or_none()
+        if not chosen_word:
+            raise HTTPException(status_code=404, detail="Word not found")
+
+        chosen_word.marked_as_learned = True
+        session.add(chosen_word)
+        await session.flush()
+        logger.debug(f"Marked word {chosen_word.lemma} is marked_as_learned: {chosen_word.marked_as_learned}.")
+
+        logger.debug(f"-----------------Updating the graph-----------------")
+        stmt = select(GraphNode).where(GraphNode.lemma == chosen_word.lemma)
+        graph_node = (await session.execute(stmt)).scalar_one_or_none()
+        if not graph_node:
+            raise HTTPException(status_code=404, detail="Graph node not found in mark_as_required()")
+
+        logger.debug(f"graph_node {graph_node.lemma} with graph_node.id {graph_node.id} is acquired:"
+                     f" {graph_node.acquired}, ")
+
+        graph_node.acquired = True
+        graph_node.mastery = 1
+
+        session.add(graph_node)
+        await session.flush()
+        await session.commit()
+
+        stmt = select(GraphNode).where(GraphNode.lemma == chosen_word.lemma)
+        graph_node = (await session.execute(stmt)).scalar_one_or_none()
+        logger.debug(f"graph_node {graph_node.lemma} is acquired: {graph_node.acquired}, "
+                     f"the new mastery is {graph_node.mastery}.")
+
+        # Check the updating in update_graph() for this change.
+        return
+    except Exception as e:
+        logger.error(f"Error marking word {word_id} as acquired: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
