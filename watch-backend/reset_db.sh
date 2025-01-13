e0a17ba8d429614295c395873b323e71a7103959
Qin Gu <guqin7@gmail.com>
2025-01-13 20:59:44 +0100

    update poetry setting and reset_db

--- watch-backend/reset_db.sh
+++ watch-backend/reset_db.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 docker compose down -v
 docker compose up -d
+
+# Wait for the database to initialize
+echo "Waiting for the database to be ready..."
+sleep 5
+
 rm -rf alembic/versions/*
 alembic revision --autogenerate -m "initial"
 alembic upgrade head
